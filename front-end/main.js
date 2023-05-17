fe15c4613008712e9efec43633460872319cfa0b
Rakesh Raj <bsrakesh@pdmrindia.com>
2023-05-17 10:04:09 +0530

    changes

--- front-end/main.js
+++ front-end/main.js
@@ -1,51 +1,90 @@
 //  GET request using fetch()
-fetch("http://localhost:3000/user")
-  // Converting received data to JSON
-  .then((response) => response.json())
-  .then((json) => {
-    // Create a variable to store HTML
-    let li = `<tr><th>id</th><th>email</th><th>firstName</th><th>lastName</th><th>Phone</th></tr>`;
-    console.log(json);
-    // Loop through each data and add a table row
-    json.forEach((user) => {
-      li += `<tr>
-                <td>${user._id} </td>
-                <td>${user.email}</td>        
-                <td>${user.firstName}</td>        
-                <td>${user.lastName}</td>        
-                <td>${user.phone}</td>   
-            </tr>`;
-    });
+getAll();
+function getAll(){
+  fetch("http://localhost:3000/user")
+    // Converting received data to JSON
+    .then((response) => response.json())
+    .then((json) => {
+      // Create a variable to store HTML
+      let li = `<tr><th>User ID</th><th>Email</th><th>First Name</th><th>Last Name</th><th>Phone Number</th></tr>`;
+      console.log(json);
+      // Loop through each data and add a table row
+      json.forEach((user) => {
+        li += `<tr>
+                  <td>${user._id} </td>
+                  <td>${user.email}</td>        
+                  <td>${user.firstName}</td>        
+                  <td>${user.lastName}</td>        
+                  <td>${user.phone}</td>   
+              </tr>`;
+      });
 
-    // Display result
-    document.getElementById("users").innerHTML = li;
+      // Display result
+      document.getElementById("users").innerHTML = li;
   });
+}
+
+
+// post user
+let postbutton = document.getElementById("post-button");
+postbutton.addEventListener('click',()=>{
+    console.log("button clicked");
+    getAll();
+});
+
+
+
 
 //  GET request using fetch()
-const getButton = document.querySelector("#get");
-const getText = document.getElementById("getText");
-
-getButton.addEventListener("click", (e) => {
-  console.log(e);
-  console.log("working");
-  const id = getText.textContent;
-  console.log(id);
-  getUser(id);
+const inputText = document.getElementById("input");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("button clicked");
+  console.log(input.value)
+  getUser(input.value);
 });
-function getUser(id) {
-  fetch(`http://localhost:3000/user/6460da1977865281b655456e`)
+
+
+function getUser(inputValue) {
+  fetch(`http://localhost:3000/user/${inputValue}`)
     // Converting received data to JSON
     .then((response) => response.json())
     .then((json) => {
       // Create a variable to store HTML
       console.log(json);
       document.getElementById("userDetails").innerHTML = `<ul>
-                        <li> user id - ${json.id}</li>
+                        <li> user id - ${json._id}</li>
                         <li> email - ${json.email}</li>
                         <li> First Name - ${json.firstName}</li>
                         <li> Last Name - ${json.lastName}</li>
                         <li> Mobile Number - ${json.phone}</li>
                     </ul>`;
+    });
+}
+
+
+
+
+//  Delete request using fetch()
+let formDelete = document.getElementById("formDelete");
+const inputTextDelete = document.getElementById("inputDelete");
+const deletedMessage = document.getElementById("deletedMessage")
+formDelete.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("button clicked");
+  console.log(inputDelete.value)
+  deleteUser(inputDelete.value);
+  getAll();
+});
+
+function deleteUser(inputValue) {
+  fetch(`http://localhost:3000/user/${inputValue}`, { method: 'DELETE' })
+    // Converting received data to JSON
+    .then((response) => response.json())
+    .then((json) => {
+      // Create a variable to store HTML
+      console.log(json.message);
+      deletedMessage.innerHTML = `<p>${json.message}<p>`;
     });
 }
 
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(
   cors({
-    origin: "http://127.0.0.1:3001",
+    origin: "http://127.0.0.1:3000",
   })
 );
 
